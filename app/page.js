48946a21065b27c3899e6f9c95b466f7c25de48b
sellfybuy <fabinhosenayt@gmail.com>
2026-02-08 07:52:25 -0400

    Add home page

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>SellFy</h1>
+      <p>Bem-vindo à plataforma.</p>
+      <a href="/dashboard">Ir para dashboard</a>
+    </main>
+  );
+}
